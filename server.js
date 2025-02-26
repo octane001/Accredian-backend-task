a42a48443332497c8bd2defd4aa556ad4405f696
@buffer! <106431224+octane001@users.noreply.github.com>
2025-02-26 17:12:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://accredian-frontend-task-d09svinyy.vercel.app/",
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
